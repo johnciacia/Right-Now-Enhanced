24254146a81da580f838f72994a5c18ca1ff6889
John Ciacia <john@voceconnect.com>
2012-02-03 08:35:02 -0500

    first commit

--- right-now-enhanced.php
+++ right-now-enhanced.php
@@ -1,26 +1,26 @@
 <?php
 /*
-	Plugin Name: Right Now Enhanced
-	Plugin URI: https://github.com/johnciacia/Right-Now-Enhanced
-	Description: Display custom post types in the "Right Now" widget.
-	Author: John Ciacia
-	Version: 1.0
-	Author URI: http://www.johnciacia.com
+Plugin Name: Right Now Enhanced
+Plugin URI: https://github.com/johnciacia/Right-Now-Enhanced
+Description: Display custom post types in the "Right Now" widget.
+Author: John Ciacia
+Version: 1.0
+Author URI: http://www.johnciacia.com
 
-	Copyright 2012  John Ciacia  (email : john@johnciacia.com)
+Copyright 2012  John Ciacia  (email : john@johnciacia.com)
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as 
-	published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 
